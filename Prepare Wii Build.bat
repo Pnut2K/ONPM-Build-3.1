f847d3da3e11664379f8d78226fcaddf240e048b
dbryce712 <dubryce@outlook.com>
2025-12-08 09:05:54 -0500

    New falco R alt, new song

--- Prepare Wii Build.bat
+++ Prepare Wii Build.bat
@@ -7,4 +7,8 @@ rmdir .\Build\Project+\Source\Netplay /s /q
 powershell.exe .\RenameFilesForWiiBuild.ps1
 ".\Build\Project+\GCTRealMate.exe" -q ".\Build\Project+\RSBE01.txt"
 ".\Build\Project+\GCTRealMate.exe" -q ".\Build\Project+\BOOST.txt"
+".\Build\Project+\GCTRealMate.exe" -q ".\Build\Project+\Source\Injects\MDEF.txt"
+".\Build\Project+\GCTRealMate.exe" -q ".\Build\Project+\Source\Injects\DEFINE.txt"
+move ".\Build\Project+\Source\Injects\MDEF.GCT" ".\Build\Project+\pf\injects\MDEF.gct"
+move ".\Build\Project+\Source\Injects\DEFINE.GCT" ".\Build\Project+\pf\injects\DEFINE.gct"
 :: powershell.exe .\ZipWiiFiles.ps1
